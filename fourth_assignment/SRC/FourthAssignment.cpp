8c9c56e9a8977aa56a116b71c19489463003bc80
GIANLUCA SCOLLO <gianlucascollo2004@gmail.com>
2026-05-27 09:45:39 +0200

    Scheletro quarto assignment

---
+++ fourth_assignment/SRC/FourthAssignment.cpp
@@ -0,0 +1,72 @@
+
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/IR/Dominators.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Argument.h"
+
+
+using namespace llvm;
+
+//-----------------------------------------------------------------------------
+// FourthAssignment implementation
+//-----------------------------------------------------------------------------
+
+namespace {
+
+  struct LoopFusion: PassInfoMixin<LoopFusion> {
+    // Main entry point, takes IR unit to run the pass on (&F) and the
+    // corresponding pass manager (to be queried if need be)
+    PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM) {
+      LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
+      DominatorTree &DT = AM.getResult<DominatorTreeAnalysis>(F);
+      
+      
+      bool changed = false;
+
+      if (changed) {
+          outs() << "La funzione " << F.getName() << " è stata modificata.\n";
+          PreservedAnalyses PA;
+          PA.preserveSet<CFGAnalyses>();
+          return PA;
+      }
+      return PreservedAnalyses::all();
+    }
+
+    // Without isRequired returning true, this pass will be skipped for functions
+    // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
+    // all functions with optnone.
+    static bool isRequired() { return true; }
+  };
+
+} // namespace
+
+
+
+//-----------------------------------------------------------------------------
+
+llvm::PassPluginLibraryInfo getFourthAssignmentPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "FourthAssignment", LLVM_VERSION_STRING,
+    [](PassBuilder &PB) {
+      PB.registerPipelineParsingCallback(
+        [](StringRef Name, FunctionPassManager &FPM,
+          ArrayRef<PassBuilder::PipelineElement>) {
+              if (Name == "loop-fusion") {
+                FPM.addPass(LoopFusion());
+                return true;
+              }
+              return false;
+          });
+    }};
+}
+
+// This is the core interface for pass plugins. It guarantees that 'opt' will
+// be able to recognize STRUCTNAME when added to the pass pipeline on the
+// command line, i.e. via '-passes=FLAGNAME'
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return getFourthAssignmentPluginInfo();
+}
