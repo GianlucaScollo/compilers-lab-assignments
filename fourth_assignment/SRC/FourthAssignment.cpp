192f989e040b52b05df65cc50b87e5de3d2d110c
GIANLUCA SCOLLO <gianlucascollo2004@gmail.com>
2026-06-01 18:24:41 +0200

    Aggiunto controllo per la condizione 1 del passo di loop fusion

--- fourth_assignment/SRC/FourthAssignment.cpp
+++ fourth_assignment/SRC/FourthAssignment.cpp
@@ -12,57 +12,108 @@ using namespace llvm;
 // FourthAssignment implementation
 //-----------------------------------------------------------------------------
 
+// struttura dati per salvare i dati relativi ad due loop adiacenti
+struct LoopPair {
+  Loop* L1 = nullptr;
+  Loop* L2 = nullptr;
+  bool areGuarded = false;
+}
+
+
+
 namespace {
 
-    struct LoopFusion: PassInfoMixin<LoopFusion> {
-        // Main entry point, takes IR unit to run the pass on (&F) and the
-        // corresponding pass manager (to be queried if need be)
-        PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM) {
-            LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
-            DominatorTree &DT = AM.getResult<DominatorTreeAnalysis>(F);
-            PostDominatorTree &PDT = AM.getResult<PostDominatorTreeAnalysis>(F);
-        
-            bool changed = false;
+  // CONDIZIONE 1
+  // controllo se i due loop sono adiacentri tra di loro
+  // questa funzione si aspetta che L1 precede L2 (e non il viceversa)
+  bool areLoopsAdjacent(Loop *L1, Loop *L2, bool areGuarded) {
 
-            for (auto *LoopIter : LI) {
-                Loop *L1 = *LoopIter; 
+    BasicBlock *EntryBlockLoop2 = L1->getExitBlock();
 
-                BasicBlock *successor = L1->getExitBlock();
+    // controllo se L1 ha un solo punto di uscita (ha dei break?)
+    // quindi se EntryBlockLoop2 = null restituisco subito false 
+    // (getExitBlock() restituisce null se esistono multiple uscite)
+    if (!EntryBlockLoop2) return false;
 
-                // controllo se il nostro loop ha un solo punto di uscita (ha dei break?)
-                if (!successor) continue;
+    BasicBlock *PreheaderLoop2 = L2->getLoopPreheader();
 
-                // controllo che il BB successore sia vuoto
-                if (!successor->getFirstNonPHIOrDbg()->isTerminator()) continue;
+    // controllo il preheder di L2, verifico che abbia un preheder (cioè che sia in 
+    // forma normale) e che esso sia vuoto (cioè che abbia solo il terminatore)
+    if (!PreheaderLoop2 || !(isBlockSafeForFusion(PreheaderLoop2))) return false;
 
-                BasicBlock *header = successor->getSingleSuccessor();
-                
-                // controllo che header esista
-                if (!header) continue;
+    // controllo se sono dei loop guarded
+    if (areGuarded) {
+      
+      // otteniamo il blocco di guardia L2
+      BasicBlock *GuardedBlockLoop2 = L2->getLoopGuardBranch()->getParent();
 
-                Loop *L2 = LI.getLoopFor(header);
+      // controllo che anche la guardia di L2 sia vuota
+      if (!(isBlockSafeForFusion(GuardedBlockLoop2))) return false;
 
-                // controllo che il L2 esista, cioè che header sia un header di un loop
-                if (!L2) continue;
-                
-                // controllo se i due loop non sono entrambi guarded oppure unguarded
-                if (L1.isGuarded() != L2.isGuarded()) continue;
-            }
+      // verifico che siano uguali
+      return (EntryBlockLoop2 == GuardedBlockLoop2);
+    }
 
-            if (changed) {
-                outs() << "La funzione " << F.getName() << " è stata modificata.\n";
-                PreservedAnalyses PA;
-                PA.preserveSet<CFGAnalyses>();
-                return PA;
-            }
-            return PreservedAnalyses::all();
-        }
+    // verifico che siano uguali
+    return (EntryBlockLoop2 == PreheaderLoop2);
+  }
+  
+  // funzione usata per controllare se i blocchi di guardia / preheder dei due loop sono vuoti
+  bool isBlockSafeForFusion(BasicBlock *BB) {
+    if (!BB) return false;
 
-        // Without isRequired returning true, this pass will be skipped for functions
-        // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
-        // all functions with optnone.
-        static bool isRequired() { return true; }
-    };
+    for (Instruction &I : *BB) {
+        // Ignoriamo le istruzioni innocue e necessarie per il CFG
+        if (I.isTerminator() || isa<PHINode>(&I) || isa<DbgInfoIntrinsic>(&I))
+            continue;
+            
+        // Permettiamo le comparazioni (necessarie se il blocco è una guardia)
+        if (isa<CmpInst>(&I))
+            continue;
+
+        // Se troviamo qualsiasi altra cosa (add, store, call, load), 
+        // c'è della logica di mezzo. Il blocco NON è sicuro/vuoto.
+        return false;
+    }
+    return true;
+  }
+
+
+
+
+  // fare controllo per capire nel caso in cui i loop sono guarded capire se hanno la stessa semantica
+  
+
+
+
+  struct LoopFusion: PassInfoMixin<LoopFusion> {
+    // Main entry point, takes IR unit to run the pass on (&F) and the
+    // corresponding pass manager (to be queried if need be)
+    PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM) {
+      LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
+      DominatorTree &DT = AM.getResult<DominatorTreeAnalysis>(F);
+      PostDominatorTree &PDT = AM.getResult<PostDominatorTreeAnalysis>(F);
+  
+      bool changed = false;
+
+      for (auto *LoopIter : LI) {
+        // ...
+      }
+
+      if (changed) {
+        outs() << "La funzione " << F.getName() << " è stata modificata.\n";
+        PreservedAnalyses PA;
+        PA.preserveSet<CFGAnalyses>();
+        return PA;
+      }
+      return PreservedAnalyses::all();
+    }
+
+    // Without isRequired returning true, this pass will be skipped for functions
+    // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
+    // all functions with optnone.
+    static bool isRequired() { return true; }
+  };
 
 } // namespace
 
